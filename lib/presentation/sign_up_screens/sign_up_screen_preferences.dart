faf971b3e3d64d263d5c00bc138013d151ed3f39
Ahmad-Shawish <ahmadshawish2001@hotmail.com>
2023-05-18 22:52:33 +0300

    Grid View

--- lib/presentation/sign_up_screens/sign_up_screen_preferences.dart
+++ lib/presentation/sign_up_screens/sign_up_screen_preferences.dart
@@ -17,26 +17,107 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
         child: Scaffold(
             backgroundColor: ColorConstant.gray50,
             resizeToAvoidBottomInset: false,
-            appBar: CustomAppBar(
-                height: getVerticalSize(42),
+            appBar: AppBar(
                 leadingWidth: 40,
-                leading: CustomImageView(
-                    svgPath: ImageConstant.imgArrowleft,
-                    height: getSize(24),
-                    width: getSize(24),
-                    margin: getMargin(left: 16, bottom: 4),
-                    onTap: () {
-                      onTapImgArrowleft(context);
-                    }),
+
                 centerTitle: true,
-                title: Text("Sign Up",
+                title: Text("Preference",
                     overflow: TextOverflow.ellipsis,
                     textAlign: TextAlign.left,
-                    style: AppStyle.txtGilroySemiBold24)),
-            body: Container(
-            color: Colors.red,
+                    style: AppStyle.txtGilroySemiBold24)
+            ),
+            body: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(65, 15, 5, 30),
+                  child: const Text(
+                    "Please Choose Your Preferred Categories:",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 50,
+                    mainAxisSpacing: 20,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: (){
+                          print("Food");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[100],
+                          child: const Text("Food"),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          print("Art & Museums");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[200],
+                          child: const Text('Art & Museums'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          print("Sports");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[300],
+                          child: const Text('Sports'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          print("Movies");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[400],
+                          child: const Text('Movies'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          print("Festivals");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[500],
+                          child: const Text('Festivals'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          print("Games");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.teal[600],
+                          child: const Text('Games'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             )
         )
+
+        /*CustomScrollView(
+
+        )*/
+
     );
   }
   onTapImgArrowleft(BuildContext context) {
